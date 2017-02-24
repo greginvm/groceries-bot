c78aa3c93cdf5b0cd9ba8de71fd3b26f25f8b41f
Ziga Stopinsek <sigi.kajzer@gmail.com>
2017-02-24 11:59:47 +0100

    db connection, default team

--- bobo/db.py
+++ bobo/db.py
@@ -1,5 +1,13 @@
 import pyrebase
 import settings
 
-firebase = pyrebase.initialize_app(settings.FIREBASE)
-db = firebase.database()
+class Model:
+	def connect(self):
+		if self.db:
+			return
+		self.firebase = pyrebase.initialize_app(settings.FIREBASE)
+		self.db = self.firebase.database()
+	
+	def __init__(self):
+		self.db = None
+
