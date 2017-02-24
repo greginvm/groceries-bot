db6de56f30c77d8d2566841aacd5d4a062e8c7db
Ziga Stopinsek <sigi.kajzer@gmail.com>
2017-02-24 12:11:23 +0100

    api placeholders

--- bobo/db.py
+++ bobo/db.py
@@ -2,12 +2,12 @@ import pyrebase
 import settings
 
 class Model:
-	def connect(self):
-		if self.db:
-			return
-		self.firebase = pyrebase.initialize_app(settings.FIREBASE)
-		self.db = self.firebase.database()
-	
-	def __init__(self):
-		self.db = None
+    def connect(self):
+        if self.db:
+            return
+        self.firebase = pyrebase.initialize_app(settings.FIREBASE)
+        self.db = self.firebase.database()
+  
+    def __init__(self):
+        self.db = None
 
