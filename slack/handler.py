f7f2ed701f724cfd1e2a0bc40215ca7481ba4b0d
Gregor Ratajc <me@gregorratajc.com>
2017-02-24 16:22:27 +0100

    refactor

--- slack/handler.py
+++ slack/handler.py
@@ -1,7 +1,7 @@
 from bobo import settings
 from bobo import db
 
-LIST_KWS = ('list', 'get')
+LISTNAMEKWS = ('list', 'get')
 ADD_KWS = ('add', 'put', 'post')
 STRIP_WORDS = (
     'to', 'what', 'who',
@@ -36,19 +36,22 @@ def _get_entity(text, cmd_kws):
 
 def trigger(user, text):
     team = settings.DEFAULT_TEAM
-    list_ = settings.DEFAULT_LIST
+    listname = settings.DEFAULT_LIST
 
     if _matches_intent(text, HELP_KWS):
-        return help_(text, user)
+        return help_()
     if _matches_intent(text, ADD_KWS):
-        return add(team, list_, text, user)
-    elif _matches_intent(text, LIST_KWS):
-        return get_all(team, list_, text, user)
-    return handle_unknown(user, text)
+        return add(team, listname, text, user)
+    elif _matches_intent(text, LISTNAMEKWS):
+        return get_all(team, listname)
+    return handle_unknown()
 
 
-def get_all(team, list_, text, user):
-    items = db.list_(team, list_)
+def get_all(team, listname):
+    items = db.list_(team, listname)
+
+    if not items:
+        return response("Nothing on the list yet")
 
     lines = []
     for _, x in items.iteritems():
@@ -58,51 +61,44 @@ def get_all(team, list_, text, user):
             'mrkdwn_in': ['text'],
         })
 
-    return respond(user, "Currently in the shopping list:", text, lines)
+    return response("Currently on the shopping list:", lines)
 
 
-def help_(text, user):
-    return respond(user, "Bobo understands the following commands:", text, [
-        {
-            "text": "add X ...",
-        },
-        {
-            "text": "list ...",
-        },
-        {
-            "text": "help",
-        },
+def help_():
+    return response("Bobo understands the following commands:", [
+        {"text": "add X ...",},
+        {"text": "list ...",},
+        {"text": "help",},
     ])
 
 
-def add(team, list_, text, user):
+def add(team, listname, text, user):
     if _matches_intent(text, VEGAN_KWS):
-        return respond(user, u"Vegetables have feelings too!", text)
+        return response(user, u"Vegetables have feelings too!", text)
 
     entity = _get_entity(text, ADD_KWS)
     if not entity:
-        return handle_unknown(user, text)
+        return handle_unknown()
 
-    db.add(team, list_, {
+    existing = db.item_exists(entity, team, listname)
+    if existing:
+        return response(user, "This item was already added by {}, thanks anyway".format(existing['user']))
+
+    db.add(team, listname, {
         'name': entity,
         'user': user,
     })
 
-    return respond(user, u"{} added, thanks {}".format(entity, user), text)
+    return response(u"{} added, thanks {}".format(entity, user))
 
 
-def handle_unknown(user, text):
-    return respond(user, "I don't understand that", text)
+def handle_unknown():
+    return response("I'm a simple piggy, I don't understand that, try harder")
 
 
-def respond(user, title, question, attachments=None, response_type="in_channel"):
+def response(title, attachments=None, response_type="in_channel"):
     if not attachments:
         attachments = []
-
-    # attachments.append({
-    #     "color": "36a64f",
-    #     "text": "Command by {}: _'{}'_".format(user, question),
-    # })
 
     return {
         "response_type": response_type,
